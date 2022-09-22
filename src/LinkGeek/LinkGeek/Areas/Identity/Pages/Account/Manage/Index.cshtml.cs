41a4a14833808bb9747d59d463b0c3e5880810ed
Caroline Reinig <creinig@microsoft.com>
2022-09-22 20:44:41 +0200

    Profile picture upload

--- src/LinkGeek/LinkGeek/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ src/LinkGeek/LinkGeek/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -2,15 +2,11 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
 using System.ComponentModel.DataAnnotations;
-using System.Security.Claims;
-using System.Text.Encodings.Web;
-using System.Threading.Tasks;
-using LinkGeek;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Net.Http.Headers;
 
 namespace LinkGeek.Areas.Identity.Pages.Account.Manage
 {
@@ -53,22 +49,21 @@ namespace LinkGeek.Areas.Identity.Pages.Account.Manage
             public string Gender { get; set; }
             
             [Display(Name = "Profile picture")]
-            public byte[] ProfilePicture { get; set; }
+            [BindProperty]
+            public IFormFile ProfilePicture { get; set; }
         }
 
         private async Task LoadAsync(ApplicationUser user)
         {
             var userName = await _userManager.GetUserNameAsync(user);
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
-            var profilePicture = await _userManager.GetProfilePicture(user);
             var gender = await _userManager.GetGender(user);
 
             Input = new InputModel
             {
                 Username = userName,
                 PhoneNumber = phoneNumber,
-                Gender = gender,
-                ProfilePicture = profilePicture
+                Gender = gender
             };
         }
 
@@ -102,51 +97,52 @@ namespace LinkGeek.Areas.Identity.Pages.Account.Manage
             if (Input.Username != userName)
             {
                 var setUserNameResult = await _userManager.SetUserNameAsync(user, Input.Username);
-                if (!setUserNameResult.Succeeded)
-                {
-                    StatusMessage = "Unexpected error when trying to set Username.";
-                    return RedirectToPage();
-                }
+                await UpdateSuccessful(user, "user name");
             }
 
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
             if (Input.PhoneNumber != phoneNumber)
             {
                 var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
-                if (!setPhoneResult.Succeeded)
-                {
-                    StatusMessage = "Unexpected error when trying to set phone number.";
-                    return RedirectToPage();
-                }
-            }
-            
-            var profilePicture = await _userManager.GetProfilePicture(user);
-            if (Input.ProfilePicture != profilePicture)
-            {
-                user.ProfilePicture = Input.ProfilePicture;
-                var update = await _userManager.UpdateAsync(user);
-                if (!update.Succeeded)
-                {
-                    StatusMessage = "Unexpected error when trying to set gender.";
-                    return RedirectToPage();
-                }
+                await UpdateSuccessful(user, "phone number");
             }
             
             
             if (Input.Gender != user.Gender)
             {
                 user.Gender = Input.Gender;
-                var update = await _userManager.UpdateAsync(user);
-                if (!update.Succeeded)
+                await UpdateSuccessful(user, "gender");
+            }
+            
+            if (Input.ProfilePicture != null) {
+                using (var ms = new MemoryStream())
                 {
-                    StatusMessage = "Unexpected error when trying to set gender.";
-                    return RedirectToPage();
+                    await Input.ProfilePicture.CopyToAsync(ms);
+                    if (ms.Length > 0 ) {
+                        var profilePictureBytes = ms.ToArray();
+                        user.ProfilePicture = profilePictureBytes;
+                        await UpdateSuccessful(user, "profile picture");
+                    }
+                    else
+                    {
+                        StatusMessage = "Unexpected error when trying to set profile picture";
+                    }
                 }
+            
             }
 
             await _signInManager.RefreshSignInAsync(user);
-            StatusMessage = "Your profile has been updated";
+            StatusMessage ??= "Your profile has been updated";
             return RedirectToPage();
+        }
+
+        public async Task UpdateSuccessful(ApplicationUser user, string updatedField)
+        {
+            var update = await _userManager.UpdateAsync(user);
+            if (!update.Succeeded)
+            {
+                StatusMessage = "Unexpected error when trying to set " + updatedField;
+            }
         }
     }
 }
