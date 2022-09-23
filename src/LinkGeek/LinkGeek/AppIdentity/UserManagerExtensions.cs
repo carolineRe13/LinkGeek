f9cd65dab0fb9a8488704893dfcb304576bc1efa
Caroline Reinig <caroline112010@hotmail.de>
2022-09-23 11:13:18 +0200

    fix merge issues

--- src/LinkGeek/LinkGeek/AppIdentity/UserManagerExtensions.cs
+++ src/LinkGeek/LinkGeek/AppIdentity/UserManagerExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using LinkGeek.AppIdentity;
+using Microsoft.AspNetCore.Identity;
 
 namespace LinkGeek;
 
--- src/LinkGeek/LinkGeek/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ src/LinkGeek/LinkGeek/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -1,15 +1,11 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
 
-using LinkGeek;
 using LinkGeek.AppIdentity;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Net.Http.Headers;
 
 namespace LinkGeek.Areas.Identity.Pages.Account.Manage
 {
