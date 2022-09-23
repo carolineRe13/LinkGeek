5f2b97345a305704b8c09a03cd20991ed5f3d24b
Caroline Reinig <creinig@microsoft.com>
2022-09-23 17:23:41 +0200

    Profile card

---
+++ src/LinkGeek/LinkGeek/Areas/ProfileCard/Pages/main.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LinkGeek.Areas.ProfileCard.Pages;
+
+public class mainModel : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
