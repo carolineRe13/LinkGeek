19af9231cf0ee35ecbe818438b51f0b0bd595328
Caroline Reinig <creinig@microsoft.com>
2022-11-06 16:31:38 +0100

    tests

--- src/LinkGeek/LinkGeek/tests/Services/ChatServiceTests.cs
+++ src/LinkGeek/LinkGeek/tests/Services/ChatServiceTests.cs
@@ -1,23 +1,38 @@
-﻿namespace LinkGeek.tests.Services
+﻿using LinkGeek.Data;
+using LinkGeek.Services;
+
+namespace LinkGeek.tests.Services
 {
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NSubstitute;
 
     [TestClass]
     public class ChatServiceTests
     {
 
+        private TestContextProvider contextProvider = new TestContextProvider();
+        private ChatService chatService;
+
         [TestInitialize]
         public void SetUp()
         {
             // common Arrange
+            this.chatService = new ChatService(this.contextProvider);
         }
 
         [TestMethod]
-        public void testName()
+        public void GetConversationAsyncTest()
         {
             // Arrange
+            // https://nsubstitute.github.io/
+            var contextMock = Substitute.For<ApplicationDbContext>();
+            contextMock.ChatMessages.Returns("");
+            contextProvider.Context = contextMock;
+
             // Act
+            var result = chatService.GetConversationAsync;
             // Assert
+            Assert.AreEqual(result,"????????");
         }
     }
 }
---
+++ src/LinkGeek/LinkGeek/tests/Services/TestContextProvider.cs
@@ -0,0 +1,14 @@
+﻿using LinkGeek.Data;
+
+namespace LinkGeek.tests.Services
+{
+    public class TestContextProvider : IContextProvider
+    {
+        public ApplicationDbContext? Context { get; set; }
+
+        public ApplicationDbContext GetContext()
+        {
+            return Context;
+        }
+    }
+}
