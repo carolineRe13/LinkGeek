d87c2ac6c705154357dc4ac6aa20a8d5d01ef3d6
Caroline Reinig <creinig@microsoft.com>
2022-11-20 21:22:27 +0100

    e2e in github actions

--- src/LinkGeek/LinkGeekTests/tests/IntegrationTests/MyProfileTests.cs
+++ src/LinkGeek/LinkGeekTests/tests/IntegrationTests/MyProfileTests.cs
@@ -1,54 +1,57 @@
-﻿using System.Diagnostics;
-using LinkGeek.Areas.ProfileCard.Pages;
-using LinkGeek.tests.Pages.Shared.SetUp;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Playwright;
+﻿using Microsoft.Playwright;
 using Xunit;
 
 namespace LinkGeek.tests.Pages.Shared;
 
-[Collection(PlaywrightFixture.PlaywrightCollection)]
 public class MyProfileTests
 {
-    private readonly PlaywrightFixture playwrightFixture;
-
-    public MyProfileTests(PlaywrightFixture playwrightFixture)
-    {
-        this.playwrightFixture = playwrightFixture;
-    }
-
     [Fact]
     public async Task EditProfile()
     {
-        var config = new ConfigurationBuilder()
-            .Add("client-secrets.json")
-            .Build();
-        
+        // Arrange
         using var playwright = await Playwright.CreateAsync();
         await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
         {
             Headless = false,
         });
         var context = await browser.NewContextAsync();
-
         var page = await context.NewPageAsync();
-
+        
+        // Act
+        // create a user
         await page.GotoAsync("https://localhost:7010/");
+        await page.GetByRole(AriaRole.Link, new() { NameString = "Register" }).ClickAsync();
+        await page.GetByLabel("Email").ClickAsync();
+        await page.GetByLabel("Email").FillAsync("linkgeek@test.com");
+        await page.GetByLabel("Username").ClickAsync();
+        await page.GetByLabel("Username").FillAsync("LinkGeekTest");
+        await page.GetByLabel("Confirm password").ClickAsync();
+        await page.GetByLabel("Confirm password").FillAsync("AVerySafePassword19*");
+        await page.Locator("#Input_Password").ClickAsync();
+        await page.Locator("#Input_Password").FillAsync("AVerySafePassword19*");
+        await page.Locator("#Input_Password").PressAsync("Enter");
+        await page.GetByRole(AriaRole.Link, new() { NameString = "Click here to confirm your account" }).ClickAsync();
         await page.GetByRole(AriaRole.Link, new() { NameString = "Login" }).ClickAsync();
-        await page.GetByRole(AriaRole.Button, new() { NameString = "Microsoft" }).ClickAsync();
-        await page.GetByRole(AriaRole.Textbox, new() { NameString = "Enter your email, phone, or Skype." }).FillAsync("caroline.reinig@gmail.com");
-        await page.GetByRole(AriaRole.Textbox, new() { NameString = "Enter your email, phone, or Skype." }).PressAsync("Enter");
-        await page.GetByRole(AriaRole.Button, new() { NameString = "Send code" }).ClickAsync();
-        await page.GetByRole(AriaRole.Textbox, new() { NameString = "Enter the code you received" }).FillAsync("6192607");
-        await page.GetByRole(AriaRole.Button, new() { NameString = "Sign in" }).ClickAsync();
-        await page.GetByRole(AriaRole.Button, new() { NameString = "Yes" }).ClickAsync();
+        await page.GetByLabel("Username").ClickAsync();
+        await page.GetByLabel("Username").FillAsync("LinkGeekTest");
+        await page.GetByLabel("Password").ClickAsync();
+        await page.GetByLabel("Password").FillAsync("AVerySafePassword19*");
+        await page.GetByRole(AriaRole.Button, new() { NameString = "Log in" }).ClickAsync();
+        
+        // go to My profile card and change status
         await page.GetByRole(AriaRole.Link, new() { NameString = "My Profile card" }).ClickAsync();
-        await page.GetByRole(AriaRole.Paragraph).Filter(new() { HasTextString = "Hey!" }).GetByRole(AriaRole.Button, new() { NameString = "" }).ClickAsync();
+        await page.Locator("id=statusPencil").ClickAsync();
         await page.Locator("#status").ClickAsync();
         await page.Locator("#status").FillAsync("Hey!!");
         await page.GetByRole(AriaRole.Button, new() { NameString = "Save changes" }).ClickAsync();
 
-
+        // Not ideal, can make tests flaky
+        await page.WaitForTimeoutAsync(1000);
+        
+        // Assert
+        var status = await page.Locator("id=userStatus").InnerTextAsync();
+        Assert.Equal("Hey!!", status);
     }
+    
+    
 }
