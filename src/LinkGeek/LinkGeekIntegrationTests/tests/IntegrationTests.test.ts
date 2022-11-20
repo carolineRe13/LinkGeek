291389ed6a2dec4dcb9495764aa9f3550fbb5f2d
carolineRe13 <caroline112010@hotmail.de>
2022-11-20 23:45:37 +0100

    Switch to node js playwright to work in github actions

---
+++ src/LinkGeek/LinkGeekIntegrationTests/tests/IntegrationTests.test.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test('Account creation test', async ({ page }) => {
+    let id = Math.floor(Math.random() * 1000);
+    await page.goto('https://localhost:7010/');
+    await page.getByRole('link', { name: 'Login' }).click();
+    await page.getByRole('navigation').getByRole('link', { name: 'Register' }).click();
+    await page.getByLabel('Email').click();
+    await page.getByLabel('Email').fill('test' + id +'@gmail.com');
+    await page.getByLabel('Email').press('Tab');
+    await page.getByLabel('Username').fill('test' + id);
+    await page.getByLabel('Username').press('Tab');
+    await page.locator('#Input_Password').fill('Tt0#' + id);
+    await page.getByLabel('Confirm password').click();
+    await page.getByLabel('Confirm password').fill('Tt0#' + id);
+    await page.getByRole('button', { name: 'Register' }).click();
+});
